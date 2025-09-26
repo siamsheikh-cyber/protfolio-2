e37fbfb0e2934c12a589a11f9bdccb018b4f95df
Siam Sheikh <siamthecoder@gmail.com>
2025-09-26 19:45:50 +0600

    color changed

--- src/component/module/project/topProject.tsx
+++ src/component/module/project/topProject.tsx
@@ -4,7 +4,7 @@ function TopProject() {
             <div className="text-center mb-[50px]">
                 <h1 className="text-2xl font-bold mb-3 block dark:hidden">Home Light Demos</h1>
                 <h1 className="text-2xl font-bold mb-3 hidden dark:block">Home Dark Demos</h1>
-                <p className="text-[#8492A6] w-[60%] mx-auto text-[14px] font-[300]">A clean and minimal personal portfolio showcasing my creative projects, professional works, and design journey.</p>
+                <p className="text-[#92a1b5] w-[60%] mx-auto text-[14px] font-[300]">A clean and minimal personal portfolio showcasing my creative projects, professional works, and design journey.</p>
             </div>
         </>
     );
